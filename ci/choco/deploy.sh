9f1f601d3f80e51e674d791bf615d9fb32a757dd
Pierce Bartine <piercebartine@gmail.com>
2018-08-07 21:52:48 -0500

    organized ci

---
+++ ci/choco/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+cd "$TRAVIS_BUILD_DIR/ci/choco"
+
+cp "$TRAVIS_BUILD_DIR/LICENSE" LICENSE.txt
+export VERSION=$(echo "$TRAVIS_TAG" | tr -d v)
+sed -i "s/VERSION/$VERSION/g" "mfaws.nuspec"
+
+curl -s https://api.github.com/repos/pbar1/mfaws/releases/$TRAVIS_TAG \
+| grep "https://github.com/pbar1/mfaws/releases/download" \
+| grep "windows" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi - -O mfaws.exe
+
+function choco(){ sudo docker run --rm -v "$(pwd)":"$(pwd)" -w "$(pwd)" linuturk/mono-choco "$@" ;}
+choco pack
+choco push -s https://push.chocolatey.org/ -k "$CHOCO_API_KEY"
