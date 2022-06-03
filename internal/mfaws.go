1fa3340a63ee3ddb0dd06a952dc330d410f5f5c5
Franky W <frankywahl@users.noreply.github.com>
2022-06-03 12:00:38 +0200

    Upgrade AWS SDK to v2

--- internal/mfaws.go
+++ internal/mfaws.go
@@ -1,14 +1,16 @@
 package internal
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+
 	"github.com/spf13/viper"
 )
 
@@ -32,11 +34,11 @@ func CheckError(err error) {
 }
 
 // CreateSession creates an AWS session from the given profile
-func CreateSession(profileLongTerm string) *session.Session {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Profile: profileLongTerm,
-	}))
-	return sess
+func CreateSession(ctx context.Context, profileLongTerm string) (aws.Config, error) {
+	return config.LoadDefaultConfig(
+		ctx,
+		config.WithSharedConfigProfile(profileLongTerm),
+	)
 }
 
 // GetMFAToken retrieves MFA token codes from either stdin or the "token" flag
@@ -53,18 +55,18 @@ func GetMFAToken() string {
 }
 
 // GetCredsWithoutRole is used to get temporary AWS credentials when NOT assuming a role
-func GetCredsWithoutRole(sess *session.Session) CredentialsShortTerm {
+func GetCredsWithoutRole(ctx context.Context, cfg aws.Config) CredentialsShortTerm {
 
 	mfaToken := GetMFAToken()
 
 	input := &sts.GetSessionTokenInput{
-		DurationSeconds: aws.Int64(viper.GetInt64("duration")),
+		DurationSeconds: aws.Int32(viper.GetInt32("duration")),
 		SerialNumber:    aws.String(viper.GetString("device")),
 		TokenCode:       aws.String(mfaToken),
 	}
 
-	svc := sts.New(sess)
-	result, err := svc.GetSessionToken(input)
+	svc := sts.NewFromConfig(cfg)
+	result, err := svc.GetSessionToken(ctx, input)
 	CheckError(err)
 	creds := result.Credentials
 
@@ -82,19 +84,24 @@ func GetCredsWithoutRole(sess *session.Session) CredentialsShortTerm {
 }
 
 // GetCredsWithRole is used to get temporary AWS credentials when assuming a role
-func GetCredsWithRole(sess *session.Session) CredentialsShortTerm {
+func GetCredsWithRole(ctx context.Context, cfg aws.Config) CredentialsShortTerm {
 
 	mfaToken := GetMFAToken()
 
-	creds := stscreds.NewCredentials(sess, viper.GetString("assume-role"), func(p *stscreds.AssumeRoleProvider) {
+	client := sts.NewFromConfig(cfg)
+
+	creds := stscreds.NewAssumeRoleProvider(client, viper.GetString("assume-role"), func(p *stscreds.AssumeRoleOptions) {
 		p.Duration = time.Duration(viper.GetInt("duration")) * time.Second
 		p.SerialNumber = aws.String(viper.GetString("device"))
-		p.TokenCode = aws.String(mfaToken)
+		p.TokenProvider = func() (string, error) {
+			return mfaToken, nil
+		}
 		p.RoleSessionName = viper.GetString("role-session-name")
 		p.ExternalID = aws.String(viper.GetString("external-id"))
+
 	})
 
-	credsRepsonse, err := creds.Get()
+	credsRepsonse, err := creds.Retrieve(ctx)
 	CheckError(err)
 	expirationTime := time.Now().UTC().Add(time.Duration(viper.GetInt("duration")) * time.Second)
 
